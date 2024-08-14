893ce59f6ce6f4a0d8bc639ecdc9c5a3c757f61a
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-08-14 10:39:43 +0200

    chore: Just a little bit of housekeeping

--- mapy/static/js/map.js
+++ mapy/static/js/map.js
@@ -2,11 +2,9 @@
  * Initialize a Leaflet map with markers for each location in the locations array.
  */
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the map element and its data attribute
     var mapElement = document.getElementById("map");
     var locations = JSON.parse(mapElement.getAttribute("data-locations"));
 
-    // Check if the locations array is not empty
     if (locations.length > 0) {
         // Initialize the map centered around the first location or default coordinates
         var initialLocation = locations[0];
@@ -21,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
 
-        // Array to store all the latlng points
         var latlngs = [];
 
         // Add a marker for each location in the locations array
@@ -32,14 +29,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     `<b>IP: ${location.ip}</b><br>Lat: ${location.latitude}<br>Lon: ${location.longitude}`
                 );
 
-            // Push the coordinates to the latlngs array
             latlngs.push([location.latitude, location.longitude]);
         });
 
         // If there is more than one location, draw a line connecting them
         if (latlngs.length > 1) {
             var polyline = L.polyline(latlngs, { color: "blue" }).addTo(map);
-            // Adjust the map's bounds to fit the polyline
             map.fitBounds(polyline.getBounds());
         }
     }
--- mapy/static/js/submit.js
+++ mapy/static/js/submit.js
@@ -4,10 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const spinner = document.getElementById("spinner");
 
     form.addEventListener("submit", function () {
-        // Disable the submit button to prevent multiple submissions
         submitButton.disabled = true;
-
-        // Show the spinner
         spinner.style.display = "inline-block";
     });
 });
